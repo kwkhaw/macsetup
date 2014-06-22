f58dbbd0a9d727383f4d84fba106690277591ba4
kkw <kwkhaw@gmail.com>
2014-06-22 17:04:58 +0800

    Added installation of homebrew cask, google-chrome, alfred and bettertouchtool

--- casual_setup.sh
+++ casual_setup.sh
@@ -1,4 +1,19 @@
-# Install brew
+# Install Homebrew
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
+# Install brew cask
+brew tap caskroom/cask
+brew install brew-cask
+
+# Install Google Chrome
+brew cask install google-chrome
+
+# Install alfred (App launcher)
+brew cask install alfred
+
+# Allowed the applications installed with homebrew cask to be launced by Alfred
+brew cask alfred link
+
+# Install Better Touch Tool
+brew cask install bettertouchtool
 
