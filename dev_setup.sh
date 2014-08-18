b432a7b8c29809f2d95473ab5b0430c831515d52
kkw <kwkhaw@gmail.com>
2014-08-18 21:56:51 +0800

    added installation of pathogen

--- dev_setup.sh
+++ dev_setup.sh
@@ -22,3 +22,12 @@ brew cask install bettertouchtool
 
 # Install node.js
 brew install node
+
+# Install npm modules
+npm install -g grunt-cli
+npm install -g coffee-script
+npm install -g bower
+npm install -g gulp
+
+# Install vim pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
